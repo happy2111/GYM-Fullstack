9edc3cb254edcd9cb9fed4804dd39e197fc2a109
Ping <muhammadyusufa2111@gmail.com>
2025-09-10 20:37:04 +0500

    Display Telegram profile photo in `ProfileInfo` component if available.

--- client/src/components/ProfileInfo.jsx
+++ client/src/components/ProfileInfo.jsx
@@ -4,7 +4,21 @@ const ProfileInfo = ({isProfileRoot}) => {
   return (
     <section className={`p-8 flex items-center gap-3 bg-dark-10 rounded-2xl mb-6 ${isProfileRoot ? "" : "max-md:hidden"}` }>
       {/*avatar*/}
-      <div className="min-w-16 h-16 font-semibold text-2xl flex items-center justify-center rounded-full overflow-hidden" style={{ backgroundColor: 'var(--color-dark-15)' }}>{authStore.user?.name[0]}</div>
+      <div
+        className="min-w-16 h-16 font-semibold text-2xl flex items-center justify-center rounded-full overflow-hidden"
+        style={{ backgroundColor: 'var(--color-dark-15)' }}
+      >
+        {authStore.user?.telegram_photo_url ? (
+          <img
+            src={authStore.user.telegram_photo_url}
+            alt="avatar"
+            className="w-full h-full object-cover"
+          />
+        ) : (
+          authStore.user?.name?.charAt(0)
+        )}
+      </div>
+
 
       {/*name  & email*/}
       <div>
