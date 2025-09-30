cd9e70438ad63963c3faa20e218b7a5037007065
Ping <muhammadyusufa2111@gmail.com>
2025-09-30 12:25:28 +0500

    Fix `onTelegramAuth` parameter in `TelegramLoginButton` script configuration

--- client/src/components/TelegramLoginButton.jsx
+++ client/src/components/TelegramLoginButton.jsx
@@ -29,7 +29,7 @@ export default function TelegramLoginButton() {
     script.src = "https://telegram.org/js/telegram-widget.js?22";
     script.setAttribute("data-telegram-login", "bullfituz_bot"); // ⚠️ username своего бота
     script.setAttribute("data-size", "large");
-    script.setAttribute("data-onauth", "onTelegramAuth"); // ⚠️ без (user)!
+    script.setAttribute("data-onauth", "onTelegramAuth(user)"); // ⚠️ без (user)!
     script.setAttribute("data-request-access", "write");
     script.async = true;
 
